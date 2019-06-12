22da7752c80978bf47bb6b630bb20da472662a2d
abzy211 <abdulwahid211@gmail.com>
2019-06-12 16:54:22 +0100

    fixed text file

---
+++ src/main/java/RunApplication.java
@@ -0,0 +1,22 @@
+/* Name: Mohammed Abdul Wahid
+ * Email: ma301ma@gold.ac.uk
+ */
+
+import java.io.FileNotFoundException;
+
+import javax.swing.JFrame;
+
+// main class to run the application 
+public class RunApplication {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		MainMenu mainMenu = new MainMenu(); // create ButtonFrame
+
+		mainMenu.setSize(550, 500); // default size for window 
+
+		mainMenu.setResizable(false); // size cannot be changed 
+		mainMenu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // default closing 
+		mainMenu.setVisible(true); // display frame
+
+	}
+}
